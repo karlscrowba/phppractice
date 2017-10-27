703c338dcd7edce58066f155b21f01646e905819
Hugo Pakula <hugo@hugopakula.com>
2017-10-27 13:55:09 -0400

    Add tutorials

---
+++ tutorials/css/index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Karl's styled page!</title>
+
+        <style>
+            body {
+                padding: 0;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+        Hey there!
+        This is my webpage, which is found at <i>http://localhost</i>.
+    </body>
+</html>
